18ae100fe6a912bfe6ecf4e9f78c871e999b4677
John Baek <jbaek7023@gmail.com>
2018-10-29 19:32:54 -0400

    initial meenu function works

---
+++ Alejandro the Survivor/Assets/Scripts/GameStarter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStarter : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //Application.LoadLevel(0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        //        Debug.Log("Start Main Scene");
+        Debug.Log("Hello World");
+        SceneManager.LoadScene("MarsLevel1");
+    }
+
+}
