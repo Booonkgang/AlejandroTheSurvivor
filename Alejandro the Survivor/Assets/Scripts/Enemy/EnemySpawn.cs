d1fb734885b2f9a7f84ed1ed485f41861c11dc41
Quang Vo <quangdvo@gmail.com>
2018-10-30 22:50:07 -0400

    Added Enemy Spawn Manager
    Added spawning of walking/shooting aliens.

---
+++ Alejandro the Survivor/Assets/Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+    
+    public GameObject enemy;
+    public float spawnTime = 10f;
+    public Transform[] spawnPoints;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+	
+	void Spawn() {
+
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
+
