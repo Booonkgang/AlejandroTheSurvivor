56b36ea61c89c9f777a0d67126d1f989195d86bc
Quang Vo <quangdvo@gmail.com>
2018-12-05 08:48:47 -0500

    Mars Game Level 2
    -Created new game level environment
    -Created moving rock formation/rock activator
    -Created destructible black rocks (only by player shooting)
    -Created moveable boulders

---
+++ Alejandro the Survivor/Assets/Scripts/LogDetection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogDetection : MonoBehaviour {
+
+    public bool logTrigger = false;
+    Animator anim;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        RockTrigger ct = other.attachedRigidbody.gameObject.GetComponent<RockTrigger>();
+        if (ct != null)
+        {
+            logTrigger = true;
+            anim.SetBool("MoveTrigger", true);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        RockTrigger ct = other.attachedRigidbody.gameObject.GetComponent<RockTrigger>();
+        if (ct != null)
+        {
+
+        }
+    }
+}
---
+++ Alejandro the Survivor/Assets/Scripts/LogHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogHealth : MonoBehaviour {
+
+    public int startingHealth = 30;
+    int currentHealth;
+
+    bool isDestroyed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakeDamage (int amount, Vector3 hitPoint)
+    {
+        if (isDestroyed)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        isDestroyed = true;
+        Destroy(gameObject, 2f);
+    }
+}
