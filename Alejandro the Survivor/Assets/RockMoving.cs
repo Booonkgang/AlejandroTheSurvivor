64cff62dfcc354d7eb509bfb74635f321f4557d7
Quang Vo <quangdvo@gmail.com>
2018-12-05 16:33:02 -0500

    Added Mars Level 4
    -Created mars level 4
    -Added moving rocks which only activate when the player moves a
    RockDetector to its collider (black flower-like object)

---
+++ Alejandro the Survivor/Assets/RockMoving.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockMoving : MonoBehaviour {
+
+    public bool rockTrigger = false;
+    Rigidbody rigidbody;
+    MeshCollider meshCollider;
+    SphereCollider sphereCollider;
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        meshCollider = gameObject.GetComponent<MeshCollider>();
+        sphereCollider = gameObject.GetComponent<SphereCollider>();
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        RockTrigger ct = other.attachedRigidbody.gameObject.GetComponent<RockTrigger>();
+        if (ct != null)
+        {
+            rockTrigger = true;
+            meshCollider.enabled = false;
+            sphereCollider.enabled = false;
+            anim.SetBool("MoveTrigger", true);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        RockTrigger ct = other.attachedRigidbody.gameObject.GetComponent<RockTrigger>();
+        if (ct != null)
+        {
+            rockTrigger = false;
+        }
+    }
+}
