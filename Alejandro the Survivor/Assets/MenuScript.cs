80fa02ec2b6bcb78d8433ec4694d7d3702b9efcd
John Baek <jbaek7023@gmail.com>
2018-12-05 18:58:45 -0500

    Menu Added to MarsLevel1

---
+++ Alejandro the Survivor/Assets/MenuScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+    private Canvas canvas;
+    private bool isFading;
+    private CanvasGroup group;
+
+	// Use this for initialization
+	void Start () {
+        canvas = GetComponent<Canvas>();
+        group = GetComponent<CanvasGroup>();
+        //Time.timeScale = 1f;
+	}
+
+    // Update is called once per frame
+    void Update () {
+        if (isFading) return;
+        if (!isFading && Input.GetKeyUp(KeyCode.Escape) && group.alpha==0f) {
+            
+            StartCoroutine(FadeFromTo(group.alpha, 1f));
+            //Time.timeScale = 0f;
+        } else if(!isFading && Input.GetKeyUp(KeyCode.Escape) && group.alpha == 1f) {
+            
+            StartCoroutine(FadeFromTo(group.alpha, 0f));
+            //Time.timeScale = 1f;
+        }
+	}
+
+    IEnumerator FadeFromTo(float from, float to) {
+        isFading = true;
+        var curve = new AnimationCurve(new Keyframe[] {
+            new Keyframe(0f, from),
+            new Keyframe(1f, to)
+        });
+
+        float time = 0f;
+        while(time<1f){
+            group.alpha = curve.Evaluate(time);
+            time += Time.deltaTime;
+
+            yield return null;
+        }
+
+        group.alpha = curve.Evaluate(1f);
+        isFading = false;
+    }
+}
---
+++ Alejandro the Survivor/Assets/ResumeScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResumeScript : MonoBehaviour {
+    private Canvas canvas;
+    private bool isFading = false;
+    private CanvasGroup group;
+
+    // Use this for initialization
+    void Start()
+    {
+        canvas = GetComponentInParent<Canvas>();
+        group = GetComponentInParent<CanvasGroup>();
+        //Time.timeScale = 1f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void resumeGame() {
+        print("resume");
+        StartCoroutine(FadeFromTo(group.alpha, 0f));
+    }
+
+    IEnumerator FadeFromTo(float from, float to)
+    {
+        isFading = true;
+        var curve = new AnimationCurve(new Keyframe[] {
+            new Keyframe(0f, from),
+            new Keyframe(1f, to)
+        });
+
+        float time = 0f;
+        while (time < 1f)
+        {
+            group.alpha = curve.Evaluate(time);
+            time += Time.deltaTime;
+
+            yield return null;
+        }
+
+        group.alpha = curve.Evaluate(1f);
+        isFading = false;
+    }
+}
